e34d3bb5b06acb9df27b77d5a5f920300053f0f8
IrinaSemenovaa <iriskaasemenova@gmail.com>
2023-07-13 10:28:39 +0300

    update HttpError.js

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,11 +1,3 @@
-// const HttpError = (status, message) => {
-//   const error = new Error(message);
-//   error.status = status;
-//   return error;
-// };
-
-// module.exports = HttpError;
-
 class HttpError extends Error {
   constructor(status, message) {
     super(message);
